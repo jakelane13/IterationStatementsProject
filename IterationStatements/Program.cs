f7530d737fe38eb75d28cfea329aa0aff939066b
Jake Lane <lanejake13@gmail.com>
2020-10-29 12:11:49 -0400

    cleaned out notes

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -8,12 +8,8 @@ namespace IterationStatements
     {
         static void Main(string[] args)
         {
-            //TODO
-
-            //Create a List called "numbers" - DONE
             var numbers = new List<int>();
 
-            //Create a variable of type int and initialize the variable with a value of 0
             var myNumber = 0;
 
             do
@@ -42,11 +38,6 @@ namespace IterationStatements
             Console.WriteLine("");
             Console.WriteLine("Decrease:");
 
-            // Create a for loop
-            // in your initializer set the value of i to 199
-            // in your conditional, as long as i is less than or equal to the amount of items in "numbers" (numbers.Count)
-            // and as long as i is greater than or equal to 0
-            // Decrement i by 1
             for(int i = 199; i <= numbers.Count && i >= 0; i--)
             {
                 Console.WriteLine($"{numbers[i]}");
